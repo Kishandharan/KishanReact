2b8e60c6c9c8fa6fda50fbb66fbcdb9a023553cd
Kishan <152014111+Kishandharan@users.noreply.github.com>
2025-09-01 18:01:45 +0530

    REACT1 done sirgit.exe add .

---
+++ rectviteapp1/src/App.jsx
@@ -0,0 +1,11 @@
+import Component1 from './CustomComponents.jsx'; 
+
+function App() {
+    return (
+        <>
+            <Component1 name="Thrupthi" age={10} gender="male"/>
+        </>
+    );    
+}
+
+export default App
