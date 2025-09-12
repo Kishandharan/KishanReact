82aeff7442494db3775cbcd6ef6f5dc15075544d
Kishan <152014111+Kishandharan@users.noreply.github.com>
2025-09-12 17:29:20 +0400

    MY FIRRRRRRRRRRRST TS PROJEEEEEEEEEEEEEEEEEEEEEEEEEEEEEECT

--- TailwindCSS_Apps/twp2/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App(){
-    return (
-        <>
-            <p class="text-center text-xl text-blue-700 hover:text-2xl hover:text-red-700 duration-500 bg-blue-200">This is tailwindcss 2 actually!! I built this without any online tutorials or resources! yay!</p>
-        </>
-    );
-}
-
-export default App
---
+++ ts_rvp/tsrvp1/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+    let var1 = "I love typescript so much more than javascript!";
+    return (
+        <>
+            <p className="text-9xl">{var1}</p>
+        </>
+    );
+}
+
+export default App
