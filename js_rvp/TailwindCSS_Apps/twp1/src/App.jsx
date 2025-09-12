e58f4ecdb135673bb3595458a403c1ca6e7366b3
Kishan <152014111+Kishandharan@users.noreply.github.com>
2025-09-12 05:36:57 +0400

    Added TS directory for TS projects

---
+++ js_rvp/TailwindCSS_Apps/twp1/src/App.jsx
@@ -0,0 +1,9 @@
+function App(){
+    return (
+        <>
+            <p className="text-2xl text-center font-black text-blue-500 hover:text-red-700 duration-500 ease-in-out">This is tailwind</p>
+        </>
+    );
+}
+
+export default App
---
+++ js_rvp/rectviteapp1/src/CustomComponents.jsx
@@ -0,0 +1,11 @@
+function Component1(props){
+    return (
+        <div>
+            <p>{props.name}</p>
+            <p>{props.age}</p>
+            <p>{props.gender}</p>
+        </div>
+    );
+}
+
+export default Component1
